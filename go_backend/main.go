009f9c5d43c6117965d14b994e56ace11da0279c
Dhanraj Kumar <dk7631333778@gmail.com>
2024-08-08 21:52:52 +0530

    config : configure datatbase for connection

--- go_backend/main.go
+++ go_backend/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/tanishadixit0206/RemotePCWakeUp/go_backend/databases"
 	"github.com/tanishadixit0206/RemotePCWakeUp/go_backend/routes"
+
 	// "go_backend/config"
 	"log"
 )
 
 func main(){
 	app := fiber.New()
+
+	databases.ConnectDB()
+	defer databases.DisconnectDB()
 
 	routes.SetUpRoutes(app)
 
