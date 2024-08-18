3f396841b22e8af5b550d14f2151fb5520a5e76b
Tanisha Dixit <dixittanisha02@gmail.com>
2024-08-18 15:39:17 +0530

    chor: modifies ui and types

--- svelte_frontend/src/lib/api/getDevices.ts
+++ svelte_frontend/src/lib/api/getDevices.ts
@@ -1,8 +1,9 @@
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
+import type { Device } from '$lib/types';
 import axios from 'axios'
-export async function getDevices():Promise<string[]>{
+export async function getDevices():Promise<Device[]>{
     try{
-        const res=await axios.get(`${PUBLIC_BACKEND_URL}`,{});
+        const res=await axios.get<Device[]>(`${PUBLIC_BACKEND_URL}`,{});
         return res.data;
     }catch(error){
         console.log(error);
--- svelte_frontend/src/routes/+page.svelte
+++ svelte_frontend/src/routes/+page.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { getDevices } from "$lib/api/getDevices";
 	import googleLogin from "$lib/api/googleLogin";
+	import type { Device } from "$lib/types";
 	import { signOut } from "@auth/sveltekit/client";
 	import { onMount } from "svelte";
 
-  let devices: string[] = [];
+  let devices: Device[] = [];
   let isLoading = true;
 
   onMount(async()=>{
     try{
-      await googleLogin();
+      // await googleLogin();
       devices=await getDevices();
       console.log(devices,JSON.stringify(devices[0]));
       isLoading=false;
@@ -25,7 +26,7 @@
       console.log(error);
     }
   }
-	function handleAddDevice() {}
+	// function handleAddDevice() {}
 	function toggleDevice( status: string): any {}
 
 </script>
@@ -33,7 +34,7 @@
  <main class="p-4">
   <h1 class="text-2xl font-bold mb-4">My Devices</h1>
   <button on:click={handleSignOut} class="bg-green-500 text-white px-4 py-2 rounded mb-4">SignOut</button>
-  <button on:click={handleAddDevice} class="bg-green-500 text-white px-4 py-2 rounded mb-4">Add Device</button>
+  <!-- <button on:click={handleAddDevice} class="bg-green-500 text-white px-4 py-2 rounded mb-4">Add Device</button> -->
 
   {#if isLoading}
     <p>Loading...</p>
@@ -41,11 +42,11 @@
     <ul>
       {#each devices as device}
         <li class="flex items-center justify-between mb-2 p-2 bg-white shadow rounded">
-          <span>{device}</span>
-          {#if device === 'off'}
-            <button on:click={() => toggleDevice(device)} class="bg-blue-500 text-white px-4 py-2 rounded">Turn On</button>
+          <span>{device.IP}</span>
+          {#if device.Status === 'On'}
+            <button on:click={() => toggleDevice(device.MAC)} class="bg-red-500 text-white px-4 py-2 rounded">Turn Off</button>
           {:else}
-            <button on:click={() => toggleDevice(device)} class="bg-red-500 text-white px-4 py-2 rounded">Turn Off</button>
+            <button on:click={() => toggleDevice(device.MAC)} class="bg-blue-500 text-white px-4 py-2 rounded">Turn On</button>
           {/if}
         </li>
       {/each}
